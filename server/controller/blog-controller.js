a30d612076e2c8c3088bdc552c5535e7b9fd8058
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-29 13:39:15 +0300

    fixed transaction bu

--- server/controller/blog-controller.js
+++ server/controller/blog-controller.js
@@ -20,22 +20,23 @@ const fetchBlogList = async (request, response) => {
 const addNewBlog = async (request, response) => {
   const { title, description } = request.body;
   const lastCreatedBlog = new Blog({ title, description, date: new Date() });
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
-    await lastCreatedBlog.save();
-  } catch (error) {
-    console.log(error);
-  }
-  try {
-    const session = await mongoose.startSession();
-    session.startTransaction();
     await lastCreatedBlog.save({ session });
-    session.commitTransaction();
+    await session.commitTransaction();
     session.endSession();
+    return response.status(200).json({ message: "Blog created successfully" });
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    console.log(error);
     return response.status(500).json({ message: error });
   }
-  return response.status(200).json({ message: "Blog created successfully" });
 };
+
 // update blog:
 const updateBlog = async (request, response) => {
   const id = request.params.id;
